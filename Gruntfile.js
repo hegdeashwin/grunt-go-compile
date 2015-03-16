661e7fa60237c79cbb8c413f9fa3a0809a523bad
hegdeashwin <ashwin.hegde3@gmail.com>
2015-03-17 01:04:06 +0530

    Updates package.json + includes JSCS

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,64 +10,74 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			all: [
+				'Gruntfile.js',
+				'tasks/**/*.js',
+				'<%= nodeunit.tests %>'
+			],
+			options: {
+				jshintrc: 'config/.jshintrc'
+			}
+		},
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp']
-    },
-
-    // Configuration to be run (and then tested).
-    go_compile: {
-      default_options: {
-        options: {
+		jscs: {
+            options: {
+                config: 'config/.jscsrc'
+            },
+            src: [
+            	'Gruntfile.js',
+				'tasks/**/*.js'
+            ]
         },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
-        }
-      }
-    },
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
-    }
+		// Before generating any new files, remove any previously-created files.
+		clean: {
+			tests: ['tmp']
+		},
 
-  });
+		// Configuration to be run (and then tested).
+		go_compile: {
+			default_options: {
+				options: {},
+				files: {
+					'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
+				}
+			},
+			custom_options: {
+				options: {
+					separator: ': ',
+					punctuation: ' !!!'
+				},
+				files: {
+					'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+				}
+			}
+		},
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+		// Unit tests.
+		nodeunit: {
+			tests: ['test/*_test.js']
+		}
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	});
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'go_compile', 'nodeunit']);
+	// Actually load this plugin's task(s).
+	grunt.loadTasks('tasks');
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-jscs');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+	// Whenever the "test" task is run, first clean the "tmp" dir, then run this
+	// plugin's task(s), then test the result.
+	grunt.registerTask('test', ['clean', 'go_compile', 'nodeunit']);
+
+	// By default, lint and run all tests.
+	grunt.registerTask('default', ['jshint', 'jscs', 'test']);
 
 };
